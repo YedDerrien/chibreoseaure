69bdf4dce0c2f8efdfc831eb67d4a93f05679132
Guillaume Monot <guillaumemonotxy@gmail.com>
2019-03-27 01:21:23 +0100

    Add delegate and data source on ViewControl table view

--- BLIH/Controllers/ViewController.swift
+++ BLIH/Controllers/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// MARK: - IBOutlet
     @IBOutlet fileprivate weak var topLabel: UILabel!
     @IBOutlet fileprivate weak var button: UIButton!
+    @IBOutlet fileprivate weak var tvRepository: UITableView!
+    @IBOutlet fileprivate weak var repositoryLabel: UITableView!
     
+    var repositoryList: [String] = ["Raytracer", "42sh", "Indie Studio", "Modotube"]
+    
+    /// MARK: - Privates
+    static fileprivate let reuseIdentifier = "reuseIdentifierVideoTableViewCell"
+    
+    /// MARK: - Application Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.prepareTableView()
         self.button.addTarget(self, action: #selector(buttonMethod), for: .touchDown)
     }
-    
 }
 
 /// ViewController : Method extension
@@ -30,4 +39,39 @@ extension ViewController {
         self.topLabel.text = "Hello YED";
         logger.verbose("come on")
     }
+}
+
+/// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    fileprivate func prepareTableView() {
+        self.tvRepository.delegate = self
+        self.tvRepository.dataSource = self
+        self.tvRepository.tableHeaderView = nil
+        self.tvRepository.estimatedRowHeight = 105
+        self.tvRepository.tableFooterView = UIView()
+        self.tvRepository.rowHeight = UITableView.automaticDimension
+        
+        /// register nib
+        self.tvRepository.registerNib(nibName: "RepoTableViewCell", reuseIdentifier: ViewController.reuseIdentifier)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositoryList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: ViewController.reuseIdentifier, for: indexPath) as? RepoTableViewCell {
+            cell.updateField(repositoryName: self.repositoryList[indexPath.row], owner: "Guillaume")
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    
 }
